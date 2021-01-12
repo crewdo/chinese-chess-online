c985bc881d16a9dd935623247779765de07bdcfd
crewdo <14520908@gm.uit.edu.vn>
2021-01-12 07:31:21 +0700

    Moving react

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const cors = require('cors')
 const app = express();
+app.use(cors());
+app.options('*', cors());
+
+
 const server = require("http").Server(app);
 const bodyParser = require("body-parser");
 
@@ -7,14 +12,24 @@ const Hall = require("./handlerRequest/Hall");
 
 const io = require("socket.io");
 const port = process.env.PORT || 888;
-const socket = io(server);
+const socket = io(server, {origins : '*:*'});
+
 
 let TheHall = new Hall(socket);
 TheHall.reception();
 
+
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
+
+
 
 server.listen(port, () =>{
     console.log("Connected to port: " + port)
 });
+
+
+// var corsOptions = {
+//     origin: '*',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
